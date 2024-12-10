55a3a0699a4bb02c849742f294057bdd57a60b48
rOyalFruit <ekdeon@gmail.com>
2024-12-10 12:40:19 +0900

    GET /,GET /about 이 브라우저에 출력되게 변경

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,17 +2,20 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public void home(){
-        System.out.println("home");
+    @ResponseBody
+    public String home(){
+        return "안녕하세요 sbb에 오신 것을 환영합니다.";
     }
 
     @GetMapping("/about")
-    public void about(){
-        System.out.println("about");
+    @ResponseBody
+    public String about(){
+        return "about page";
     }
 }
