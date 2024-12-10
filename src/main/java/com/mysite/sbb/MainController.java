f719fcf2104a909064d3de6673fb165279ff674a
rOyalFruit <ekdeon@gmail.com>
2024-12-10 10:37:27 +0900

    GET /, GET /about

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,18 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public void home(){
+        System.out.println("home");
+    }
+
+    @GetMapping("/about")
+    public void about(){
+        System.out.println("about");
+    }
+}
