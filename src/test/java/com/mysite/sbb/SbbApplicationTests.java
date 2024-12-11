7344586bf7039afd2ffb312032494fa7efe20176
rOyalFruit <ekdeon@gmail.com>
2024-12-11 11:24:18 +0900

    답변 데이터를 통해 질문 데이터 찾기

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,9 +1,11 @@
 package com.mysite.sbb;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,11 +20,19 @@ class SbbApplicationTests {
     @Autowired
     private AnswerRepository answerRepository;
 
+    // 테스트 코드에서는 QuestionRepository가 findById 메서드를 통해 Question 객체를 조회하고 나면 DB 세션이 끊어짐.
+    // @Transactional을 사용하면 메서드가 종료될 때까지 DB 세션이 유지됨.
+    // or @OneToMany, @ManyToOne의 옵션으로 fetch=FetchType.LAZY 또는 fetch=FetchType.EAGER처럼 가져오는 방식을 설정하면 됨.
+    @Transactional
     @Test
     void testJpa(){
-        Optional<Answer> oa = this.answerRepository.findById(1);
-        assertTrue(oa.isPresent());
-        Answer a = oa.get();
-        assertEquals(2, a.getQuestion().getId());
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getConent());
     }
 }
