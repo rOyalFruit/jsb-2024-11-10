4e527773e380cbef2a589be41848967d37e4a629
rOyalFruit <ekdeon@gmail.com>
2024-12-12 14:08:51 +0900

    root URL 설정

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,20 +2,12 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    @ResponseBody
-    public String home(){
-        return "안녕하세요 sbb에 오신 것을 환영합니다.";
-    }
-
-    @GetMapping("/about")
-    @ResponseBody
-    public String about(){
-        return "about page";
+    public String root(){
+        return "redirect:/question/list";
     }
 }
