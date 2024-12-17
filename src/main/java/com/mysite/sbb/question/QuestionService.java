6729e486f588cc5f092ea79fd8ef36a0f119f570
rOyalFruit <ekdeon@gmail.com>
2024-12-17 15:53:48 +0900

    질문 삭제 기능 추가

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -59,4 +59,8 @@ public class QuestionService {
 
         questionRepository.save(question);
     }
+
+    public void delete(Question question) {
+        questionRepository.delete(question);
+    }
 }
